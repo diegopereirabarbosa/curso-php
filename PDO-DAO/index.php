07c6bf71dd5227e3e0446e9a2231caecd096d312
programador_web_Diego <diego@camptech.com.br>
2019-02-12 16:35:44 -0200

    Insert e delet com DAO

--- PDO-DAO/index.php
+++ PDO-DAO/index.php
@@ -1,11 +1,48 @@
 <?php 
 
 require_once("config.php");
+//Carrega um usuario.
+//$root = new Usuario();
+//$root->loadById(15);
+//echo $root; 
 
-$root = new Usuario();
-$root->loadById(10);
+//Carrega uma lista completa
+//$lista = Usuario ::getList();
+//echo json_encode($lista);
 
-echo $root;
+
+//busca uma lista de usuarios pelo login
+//$search = Usuario::search("pe");
+//echo json_encode($search);
+
+//Carregar login e senha
+//$usuario = new Usuario();
+//$usuario->login("ana", "abrc5");
+//echo $usuario;
 
 
+//insere um novo usuario
+//$aluno = new Usuario();
+//$aluno->setDeslogin("Prado");
+//$aluno->setDessenha("1234");
+//$aluno->insert();
+//echo $aluno;
+
+//Update atualiza usuario
+//$usuario = new Usuario();
+//$usuario->loadById(1);
+//$usuario->update("André", "123456");
+//echo $usuario;
+
+//Deletar registro
+$usuario = new Usuario();
+$usuario->loadById(1);
+$usuario->delete();
+echo $usuario;
 ?>
+
+
+
+
+
+
---
+++ copia DB/copia banco.sql
@@ -0,0 +1,87 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: curso_php
+-- ------------------------------------------------------
+-- Server version	5.7.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_usuario`
+--
+
+DROP TABLE IF EXISTS `tb_usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_usuario` (
+  `idusuario` int(11) NOT NULL AUTO_INCREMENT,
+  `deslogin` varchar(64) NOT NULL,
+  `dessenha` varchar(256) NOT NULL,
+  `dtcadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idusuario`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_usuario`
+--
+
+LOCK TABLES `tb_usuario` WRITE;
+/*!40000 ALTER TABLE `tb_usuario` DISABLE KEYS */;
+INSERT INTO `tb_usuario` VALUES (2,'Carlos','1234','2019-02-12 17:59:31'),(3,'Aldo','1234','2019-02-12 17:59:50'),(4,'Ana','1234','2019-02-12 17:59:59'),(5,'Prado','123456','2019-02-12 18:00:09');
+/*!40000 ALTER TABLE `tb_usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'curso_php'
+--
+
+--
+-- Dumping routines for database 'curso_php'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `sp_usuario_insert` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_usuario_insert`(
+pdeslogin varchar(64),
+pdessenha varchar(256)
+)
+BEGIN
+INSERT INTO tb_usuario(deslogin, dessenha) VALUE(pdeslogin, pdessenha);
+
+SELECT * FROM tb_usuario WHERE idusuario = LAST_INSERT_ID(); 
+
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-02-12 16:29:28
